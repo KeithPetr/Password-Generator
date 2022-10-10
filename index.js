118d31f54c09035c72181bd39263f21eeeb65175
KeithPetr <keith.petryshyn@gmail.com>
2022-10-10 14:33:26 -0300

    changed layout added options

--- index.js
+++ index.js
@@ -1,51 +1,67 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L",
-"M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b",
-"c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r",
-"s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6",
- "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")",
- "_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+const uppercaseChar = ["A","B","C","D","E","F","G","H","I","J","K","L",
+    "M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+ const lowercaseChar = ["a","b",
+    "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r",
+    "s","t","u","v","w","x","y","z"]
+ const numbers = [ "0", "1", "2", "3", "4", "5", "6","7", "8", "9"]
+ const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")",
+    "_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
 
-let passwordOne = document.getElementById('password-1');
-let passwordTwo = document.getElementById('password-2');
+let password = document.getElementById('password');
+let passwordLength = document.getElementById('length-el');
+let uppercaseCheck = document.getElementById('uppercase')
+let lowercaseCheck = document.getElementById('lowercase')
+let numbersCheck = document.getElementById('numbers')
+let symbolsCheck = document.getElementById('symbols')
+let generateBtn = document.getElementById('generateBtn-el')
+let copyBtn = document.getElementById('copy-btn')
 
-function generate() {
+let newPassword = []
 
-    passwordOne.textContent = ""
-    passwordTwo.textContent = ""
 
-    for (let i = 0; i < 16; i++) {
-        let randPassOne = Math.floor(Math.random() * 91)
-        let randPassTwo = Math.floor(Math.random() * 91)
-        passwordOne.textContent += characters[randPassOne]
-        passwordTwo.textContent += characters[randPassTwo]
-    }
+function getUppercase() {
+    return uppercaseChar[Math.floor(Math.random() * uppercaseChar.length)]
 }
 
-function copyOne() {
+console.log(getUppercase())
+
+function getLowercase() {
+    return lowercaseChar[Math.floor(Math.random() * lowercaseChar.length)]
+}
+
+console.log(getLowercase())
+
+function getNumbers() {
+    return numbers[Math.floor(Math.random() * numbers.length)]
+}
+
+console.log(getNumbers())
+
+function getSymbols() {
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+console.log(getSymbols())
+
+function generatePassword() {
+    const length = passwordLength.value
+    password = ""
+}
+
+function copy() {
   
     // Select the text field
-    passwordOne.select();
-    passwordOne.setSelectionRange(0, 99999); // For mobile devices
+    password.select();
+    password.setSelectionRange(0, 99999); // For mobile devices
   
      // Copy the text inside the text field
-    navigator.clipboard.writeText(passwordOne.value);
+    navigator.clipboard.writeText(password.value);
   
     // Alert the copied text
     alert("Copied the text!");
 }
 
-function copyTwo() {
-  
-    // Select the text field
-    passwordTwo.select();
-    passwordTwo.setSelectionRange(0, 99999); // For mobile devices
-  
-     // Copy the text inside the text field
-    navigator.clipboard.writeText(passwordTwo.value);
-  
-    // Alert the copied text
-    alert("Copied the text!");
-}
+copyBtn.addEventListener("click", copy)
 
 
   
