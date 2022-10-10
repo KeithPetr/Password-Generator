2bcf0a412d92cbc5f91139f76f5479e02aacb9f9
KeithPetr <keith.petryshyn@gmail.com>
2022-10-10 20:08:35 -0300

    changed the array of symbols

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const uppercaseChar = ["A","B","C","D","E","F","G","H","I","J","K","L",
     "c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r",
     "s","t","u","v","w","x","y","z"]
  const numbers = [ "0", "1", "2", "3", "4", "5", "6","7", "8", "9"]
- const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")",
-    "_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+ const symbols = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")",
+  "_", "+", "|"]
 
 let passwordEl = document.getElementById('password-el');
 let passwordLength = document.getElementById('length-el');
